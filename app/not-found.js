3cc52d0c18b47b1dc656d3967f9af48bb3ba0403
JinXzAF <arianf2004@gmail.com>
2023-07-27 22:43:29 -0400

    testing changes

--- app/not-found.js
+++ app/not-found.js
@@ -1,22 +1,10 @@
 "use client";
 
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
-
 export default function NotFound() {
-  // This page is not shown in production, only in dev
-  // useEffect(() => {
-  //   redirect("/");
-  // }, []);
-
   return (
     <div>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <p>
-        View <Link href="/blog">all posts</Link>
-      </p>
     </div>
   );
 }
